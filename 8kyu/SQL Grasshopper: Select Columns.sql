5e22ac0b59316eace925ad93830ba327dfec97d9
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-01-31 17:44:09 +0100

    Create SQL Grasshopper: Select Columns.sql

---
+++ 8kyu/SQL Grasshopper: Select Columns.sql
@@ -0,0 +1,10 @@
+/* #Greetings Grasshopper!# Using only SQL, write a query that returns all rows in the custid, custname, and custstate columns from the customers table.
+
+###Table Description for customers:###
+
+Your solution should contain only SQL.
+*/
+
+--Solution
+SELECT custid, custname, custstate
+FROM customers  
