dc55fc96ea598689b80006e8d036b449cd231543
Giacomo001 <giacomo.leoni1992@gmail.com>
2019-01-31 17:25:57 +0100

    Create SQL Basics: Simple SUM.sql

---
+++ 8kyu/SQL Basics: Simple SUM.sql
@@ -0,0 +1,19 @@
+/* For this challenge you need to create a simple SUM statement that will sum all the ages.
+
+People table schema
+
+id
+name
+age
+
+Select table schema
+age_sum (sum of ages)
+
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+*/
+
+--Solution
+SELECT 
+  SUM(age) AS age_sum 
+FROM people
+
